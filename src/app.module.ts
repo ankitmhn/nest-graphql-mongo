187b5dbb2df50fed96468b88a7d276d2df58ec25
Ankit <find.ankit@gmail.com>
2021-11-28 23:45:37 +0530

    setup dotenv for mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
-
+config();
+console.log('Mongodb: ', process.env.MONGODB_URI);
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       playground: true,
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     AuthorModule,
     BookModule,
   ],
